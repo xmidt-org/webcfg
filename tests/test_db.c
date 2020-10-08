06153795119cecd335d3302e5a15928d6973d234
Sreedevi V <Sreedevi_V@cable.comcast.com>
2020-10-08 09:52:58 +0530

    Adding unit test

--- tests/test_db.c
+++ tests/test_db.c
@@ -1,4 +1,4 @@
-/**
+ /**
   * Copyright 2019 Comcast Cable Communications Management, LLC
   *
   * Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,6 +22,146 @@
 #include "../src/webcfg_pack.h"
 #include "../src/webcfg_helpers.h"
 #include "../src/webcfg_param.h"
+#include "../src/webcfg_multipart.h"
+#define UNUSED(x) (void )(x)
+//mock functions
+
+char * webcfg_appendeddoc(char * subdoc_name, uint32_t version, char * blob_data, size_t blob_size, uint16_t *trans_id)
+{
+UNUSED(subdoc_name);
+UNUSED(version);
+UNUSED(blob_data);
+UNUSED(blob_size);
+UNUSED(trans_id);
+return;
+}
+
+void initEventHandlingTask(){
+return 0;};
+
+void processWebcfgEvents(){
+return 0;
+};
+
+
+WEBCFG_STATUS checkAndUpdateTmpRetryCount(webconfig_tmp_data_t *temp, char *docname)
+{
+UNUSED(temp);
+UNUSED(docname);
+return;
+}
+void setValues(const param_t paramVal[], const unsigned int paramCount, const int setType, char *transactionId, money_trace_spans *timeSpan, WDMP_STATUS *retStatus, int *ccspStatus)
+{
+	UNUSED(paramVal);
+	UNUSED(paramCount);
+	UNUSED(setType);
+	UNUSED(transactionId);
+	UNUSED(timeSpan);
+	UNUSED(retStatus);
+	UNUSED(ccspStatus);
+	return;
+}
+
+void addWebConfgNotifyMsg(char *docname, uint32_t version, char *status, char *error_details, char *transaction_uuid, uint32_t timeout,char* type, uint16_t error_code, char *root_string, long response_code)
+{
+UNUSED(docname);
+UNUSED(version);
+UNUSED(status);
+UNUSED(error_details);
+UNUSED(transaction_uuid);
+UNUSED(timeout);
+UNUSED(type);
+UNUSED(error_code);
+UNUSED(root_string);
+UNUSED(response_code);
+return;
+}
+WDMP_STATUS mapStatus(int ret)
+{
+UNUSED(ret);
+return;
+}
+
+
+void isSubDocSupported(){
+return 0;};
+
+void checkAkerStatus(){
+return 0;};
+void updateAkerMaxRetry(webconfig_tmp_data_t *temp, char *docname)
+{
+UNUSED(temp);
+UNUSED(docname);
+return;
+}
+
+void processAkerSubdoc(){
+return 0;};
+
+
+char * getsupportedDocs()
+{
+return;
+}
+char * getsupportedVersion()
+{
+return;
+}
+
+char * getDeviceBootTime()
+{
+	char *bTime = strdup("152200345");
+	return bTime;
+}
+
+char * getFirmwareVersion()
+{
+	char *fName = strdup("Firmware.bin");
+	return fName;
+}
+
+int getForceSync(char** pString, char **transactionId)
+{
+	UNUSED(pString);
+	UNUSED(transactionId);
+	return 0;
+}
+
+char * getProductClass()
+{
+	char *pClass = strdup("Product");
+	return pClass;
+}
+
+char * getModelName()
+{
+	char *mName = strdup("Model");
+	return mName;
+}
+
+
+void retryMultipartSubdoc(){
+return 0;};
+char *get_global_systemReadyTime()
+{
+	char *sTime = strdup("158000123");
+	return sTime;
+}
+
+char * getRebootReason()
+{
+	char *reason = strdup("factory-reset");
+	return reason;
+}
+
+char* get_global_auth_token(){
+return ;
+}
+void getCurrent_Time(struct timespec *timer){
+UNUSED(timer);
+return;
+}
+
 
 
 void test_blobPack(){
@@ -51,7 +191,6 @@ void test_blobPack(){
 	size_t len = 0;
  	webcfgdbBlobPackSize = webcfgdb_blob_pack(webcfgdb, webcfgtemp, &data);
 	//inside the function
-
 	db_data = webcfgdb;
     	temp_data = webcfgtemp;
 	CU_ASSERT_PTR_NOT_NULL(db_data);
@@ -69,7 +208,7 @@ void test_blobPack(){
 	printf("\ndm->name %s\n",dm->name);
 	printf("dm->version %d\n",dm->version);
 	CU_ASSERT_PTR_NOT_NULL(dm);
-	
+	//CU_ASSERT_STRING_EQUAL(dm->name,"root");
 
 }
 
