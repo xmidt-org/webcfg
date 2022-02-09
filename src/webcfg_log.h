ff307968ffbe8780c8d69b3a75ae499117c64d9a
Shilpa Seshadri <s.shilpa24@gmail.com>
2022-02-09 21:52:41 +0530

    Define cimplog rdk_logger_module and func

--- src/webcfg_log.h
+++ src/webcfg_log.h
@@ -19,9 +19,19 @@
 
 #define WebConfigLog(...)       __cimplog_rdk_generic(WEBCFG_RDK_LOGGING_MODULE, WEBCFG_LOGGING_MODULE, LEVEL_INFO, __VA_ARGS__)
 
+#ifdef WEBCONFIG_BIN_SUPPORT
+
+#define WebcfgError(...)	cimplog_error(WEBCFG_LOGGING_MODULE, __VA_ARGS__)
+#define WebcfgInfo(...)		cimplog_info(WEBCFG_LOGGING_MODULE, __VA_ARGS__)
+#define WebcfgDebug(...)	cimplog_debug(WEBCFG_LOGGING_MODULE, __VA_ARGS__)
+
+#else
+
 #define WebcfgError(...)	__cimplog_rdk_generic(WEBCFG_RDK_LOGGING_MODULE, WEBCFG_LOGGING_MODULE, LEVEL_ERROR, __VA_ARGS__)
 #define WebcfgInfo(...)		__cimplog_rdk_generic(WEBCFG_RDK_LOGGING_MODULE, WEBCFG_LOGGING_MODULE, LEVEL_INFO, __VA_ARGS__)
 #define WebcfgDebug(...)	__cimplog_rdk_generic(WEBCFG_RDK_LOGGING_MODULE, WEBCFG_LOGGING_MODULE, LEVEL_DEBUG, __VA_ARGS__)
+
+#endif
 
 #else
 #define WebConfigLog(...)       printf(__VA_ARGS__)
