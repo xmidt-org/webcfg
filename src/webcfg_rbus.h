e354ba14a83f77313d84c7912a990a221404b6f1
Shilpa Seshadri <shilpa_seshadri@comcast.com>
2022-11-09 23:54:15 +0530

    Use rdkcentral RBUS repo & resolve dependencies

--- src/webcfg_rbus.h
+++ src/webcfg_rbus.h
@@ -26,8 +26,6 @@
 #include <rbus/rbus_object.h>
 #include <rbus/rbus_property.h>
 #include <rbus/rbus_value.h>
-#include <rbus-core/rbus_core.h>
-#include <rbus-core/rbus_session_mgr.h>
 
 #include <wdmp-c.h>
 #include <wrp-c.h>
